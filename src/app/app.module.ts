e3b01b506cad8c137cd6a7852d88be0b3a35aa0b
Rashmi <Rashmi@tripund.co.in>
2026-04-02 17:13:07 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule
   ],
   providers: [],
